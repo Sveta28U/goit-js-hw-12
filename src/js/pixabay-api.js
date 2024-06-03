5046fdd539f08f6176f4db9ef38188fed7e0757e
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-06-03 15:27:55 +0300

    commit 12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,25 @@
+import axios from 'axios';
 const API_KEY = '22356210-f5a6fb995cd777b2b01184cc9';
 const BASE_URL = 'https://pixabay.com/api/';
-
-export function getPhotos(query) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  return fetch(`${BASE_URL}/?${params}`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json('');
-  });
+export const userOptions = {
+  page: 1,
+  per_page: 15,
+  query: null,
+};
+export async function getPhotos() {
+  try {
+    return await axios.get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: userOptions.query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: userOptions.page,
+        per_page: userOptions.per_page,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/main.js
+++ src/main.js
@@ -7,45 +7,88 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { getPhotos } from './js/pixabay-api';
+import { getPhotos, userOptions } from './js/pixabay-api';
 import { creatMarkup } from './js/render-functions';
 const form = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.load-more ');
 form.addEventListener('submit', onSubmit);
+
+loadMoreBtn.addEventListener('click', onClick);
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-function onSubmit(event) {
+async function onSubmit(event) {
   event.preventDefault();
+  userOptions.page = 1;
   galleryList.innerHTML = '';
   showLoader();
-  const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
-  getPhotos(searchQuery)
-    .then(res => {
-      if (res.hits.length === 0) {
-        iziToast.error({
-          position: 'topRight',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
+  hideBtn();
+  userOptions.query = event.currentTarget.elements.searchQuery.value.trim();
+  try {
+    const { data } = await getPhotos();
+    if (data.hits.length === 0) {
+      iziToast.error({
+        position: 'topRight',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    }
 
-      galleryList.innerHTML = creatMarkup(res.hits);
-      lightbox.refresh();
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
-      hideLoader();
-      event.target.reset();
+    galleryList.innerHTML = creatMarkup(data.hits);
+    lightbox.refresh();
+    if (data.totalHits > userOptions.per_page) {
+      showBtn();
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+    event.target.reset();
+  }
+}
+async function onClick() {
+  userOptions.page += 1;
+  showLoader();
+  try {
+    const { data } = await getPhotos();
+    galleryList.insertAdjacentHTML('beforeend', creatMarkup(data.hits));
+    lightbox.refresh();
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
     });
+    const lastPage = Math.ceil(data.totalHits / userOptions.per_page);
+    if (lastPage === userOptions.page) {
+      hideBtn();
+      iziToast.info({
+        position: 'topRight',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
 }
 function showLoader() {
   loader.classList.remove('is-hidden');
 }
 function hideLoader() {
   loader.classList.add('is-hidden');
+}
+
+function showBtn() {
+  loadMoreBtn.classList.remove('is-hidden');
+}
+function hideBtn() {
+  loadMoreBtn.classList.add('is-hidden');
 }
